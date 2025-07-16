ae9c402ba18c767ec75d284cb99d246559a01100
Josias Hod <55164489+josiashod@users.noreply.github.com>
2025-07-16 11:11:10 +0200

    Update building.h

--- core/building.h
+++ core/building.h
@@ -9,6 +9,7 @@ public:
     Building(long long id, const std::vector<QPointF>& points, QGraphicsItem* parent = nullptr);
 
     QRectF boundingRect() const override;
+    QPainterPath shape() const override;
 
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = nullptr) override;
 
